eec63ace18617e1b273546effb1e555e91b0f883
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-12-09 16:55:02 +0000

    autoComplete app basic

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState([]);
+  const [text, setText] = useState([]);
+  const [suggestions, setSuggetions] = useState([]);
+  useEffect(() => {
+    getname();
+  }, []);
+
+  const getname = async () => {
+    const response = await axios.get('https://reqres.in/api/users');
+    setName(response.data.data);
+  }
+
+  const onSuggetionHandler = (fname) => {
+    setText(fname);
+    setSuggetions([]);
+  }
+
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text) {
+      matches = name.filter(fn => {
+        const regex = new RegExp(`${text}`, "gi");
+        return fn.first_name.match(regex)
+      })
+    }
+    setSuggetions(matches);
+    setText(text)
+  }
+
+
+  return (
+    <div>
+      <label> Search </label>
+      <input type="text" size="50"
+        onChange={e => onChangeHandler(e.target.value)}
+        value={text}
+        onBlur={() => { // add delay because onBlur fires before onClick
+          setTimeout(() => {
+            setSuggetions([])
+          }, 100);
+        }}
+
+      />
+      {suggestions && suggestions.map((suggestions, i) =>
+        <div className="suggestions" key={i}
+          onClick={() => onSuggetionHandler(suggestions.first_name)} // set value in inputbox, empty sugg list
+        >{suggestions.first_name}</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+//  Memorization is never free, we are trading space for time. 
