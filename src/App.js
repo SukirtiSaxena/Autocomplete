73f410956d65494035c46fb162de67d6afa2758c
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-12-10 17:46:13 +0000

    trying searchtext as dependency in useEffect

--- src/App.js
+++ src/App.js
@@ -3,62 +3,44 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState([]);
-  const [text, setText] = useState([]);
-  const [suggestions, setSuggetions] = useState([]);
+
+  const [names, setNames] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
   useEffect(() => {
-    getname();
-  }, []);
 
-  const getname = async () => {
-    const response = await axios.get('https://reqres.in/api/users');
-    setName(response.data.data);
-  }
-
-  const onSuggetionHandler = (fname) => {
-    setText(fname);
-    setSuggetions([]);
-  }
-
-  const onChangeHandler = (text) => {
-    let matches = []
-    if (text) {
-      matches = name.filter(fn => {
-        const regex = new RegExp(`${text}`, "gi");
-        return fn.first_name.match(regex)
-      })
+    const getname = async () => {
+      if (searchText) {
+        const response = await axios.get('https://api.disneyapi.dev/characters');
+        let newList = response.data.data.filter(l => {
+          return l.name.includes(searchText);
+        })
+        setNames(newList);
+      }
     }
-    setSuggetions(matches);
-    setText(text)
+    getname();
+  }, [searchText]);
+
+  const onSearchTextChange = (e) => {
+    setSearchText(e.target.value);
   }
 
+/*   const onClickList = (e) => {
+    setNames([]);
+  } */
 
   return (
     <div>
-      <label> Search </label>
-      <input type="text" size="50"
-        onChange={e => onChangeHandler(e.target.value)}
-        value={text}
-        onBlur={() => { // add delay because onBlur fires before onClick
-          setTimeout(() => { setSuggetions([]) }, 200);
-        }}
-      />
-      <Suggestions suggestions={suggestions} onSuggetionHandler={onSuggetionHandler} />
+      <input type="text" value={searchText}
+        onChange={onSearchTextChange} />
+      {names.map(n => (
+        <div key={n._id} >{n.name} </div>
+      )
+      )}
 
     </div>
-  );
-}
-
-const Suggestions =   ({suggestions, onSuggetionHandler}) => {
-return (
-    <>
-      {suggestions.map((suggestions, i) =>
-        <div className="suggestions" key={i}
-          onClick={() => onSuggetionHandler(suggestions.first_name)} // set value in inputbox, empty sugg list
-        >{suggestions.first_name}</div>
-      )}
-    </>
   )
+
 };
 
 export default App;
