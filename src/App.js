398802d07048591a739a6577d3ab800f5e5c7472
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-12-10 19:22:41 +0000

    useRef  for list disappearance on selection

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  let select = useRef(false);
 
   const [names, setNames] = useState([]);
   const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-
     const getname = async () => {
-      if (searchText) {
+      if (searchText === null || searchText === '')
+        setNames([]);
+      if (searchText && !select.current) {
         const response = await axios.get('https://api.disneyapi.dev/characters');
         let newList = response.data.data.filter(l => {
           return l.name.includes(searchText);
         })
         setNames(newList);
       }
+
+      select.current = false;
     }
     getname();
-  }, [searchText]);
+  }, [searchText, select]);
 
   const onSearchTextChange = (e) => {
     setSearchText(e.target.value);
   }
 
-/*   const onClickList = (e) => {
+  const onClickList = (e) => {
+    select.current = true;
     setNames([]);
-  } */
+    setSearchText(e);
+  }
 
   return (
     <div>
       <input type="text" value={searchText}
         onChange={onSearchTextChange} />
       {names.map(n => (
-        <div key={n._id} >{n.name} </div>
+        <div key={n._id} onClick={e => { onClickList(n.name) }}>{n.name} </div>
       )
       )}
 
